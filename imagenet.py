33f40147c4089e3da41185e90f984332fc9f0495
Vikhyat Agrawal <82029380+vikhyatt@users.noreply.github.com>
2024-09-27 10:21:30 +0200

    Update imagenet.py

--- imagenet.py
+++ imagenet.py
@@ -4,12 +4,12 @@ from PIL import Image
 import numpy as np
 
 # Load your Arrow dataset
-dataset_path = './data/imagenet-1k/default/1.0.0/07900defe1ccf3404ea7e5e876a64ca41192f6c07406044771544ef1505831e8/'
+dataset_path = '../data/imagenet-1k/default/1.0.0/07900defe1ccf3404ea7e5e876a64ca41192f6c07406044771544ef1505831e8/'
 train_arrow_files = [f'{dataset_path}/imagenet-1k-train-000{i:02d}-of-00257.arrow' for i in range(257)]
 val_arrow_file = [f'{dataset_path}/imagenet-1k-validation-000{i:02d}-of-00013.arrow' for i in range(13)]
 
 # Create directories for train/val splits
-output_dir = 'data/imagenet/'
+output_dir = '../data/imagenet/'
 train_dir = os.path.join(output_dir, 'train')
 val_dir = os.path.join(output_dir, 'val')
 os.makedirs(train_dir, exist_ok=True)
